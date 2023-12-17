e1083bae824a9fddca96857cf55f29e65fb5f1f6
omicron <131537767+omicr0nn@users.noreply.github.com>
2023-12-17 23:58:44 +0300

    Update ipquery.py

--- ipquery.py
+++ ipquery.py
@@ -40,36 +40,52 @@ def countdown(seconds):
 
     print("\nCountdown complete!")
 
-def ipİnfo():
+def menu_start():
     clear_screen()
     print(menu)
-    ipaddress = input("Enter the any Ip Address: \033[0m")
-    fields = [
-        'status', 'continent', 'continentCode', 'country', 'countryCode',
-        'region', 'regionName', 'city', 'district', 'zip', 'lat', 'lon',
-        'timezone', 'isp', 'org', 'as', 'asname', 'reverse', 'mobile',
-        'proxy', 'hosting', 'query'
-    ]
-    url = f"http://ip-api.com/json/{ipaddress}?fields={','.join(fields)}"
 
-    response = requests.get(url, headers=headers) # Make Requests
-    data = response.json()
-    data['author'] = 'omicr0nn'
-    file_name = ipaddress + ".json"
+def ipİnfo():
+    menu_start()
+    choose = int(input("\033[36mPlease select an option: \033[0m"))
 
-    if response.status_code == 200:
-        print("\033[35mRequests successfully")
-        try:
-            # Create and Save File
-            with open(file_name, "w") as json_file:
-                json.dump(data, json_file, indent=4)
-                print(f"\033[32mThe file named {file_name} has been saved")
-        except:
-            print("\033[31mFile creation failed")
-    else:
-        print("\033[31mRequest failed")
-    
-    countdown(5)
+    if choose == 1:
+        ipaddress = input("\033[36mEnter the any Ip Address: \033[0m")
+        fields = [
+            'status', 'continent', 'continentCode', 'country', 'countryCode',
+            'region', 'regionName', 'city', 'district', 'zip', 'lat', 'lon',
+            'timezone', 'isp', 'org', 'as', 'asname', 'reverse', 'mobile',
+            'proxy', 'hosting', 'query'
+        ]
+        url = f"http://ip-api.com/json/{ipaddress}?fields={','.join(fields)}"
+
+        response = requests.get(url, headers=headers) # Make Requests
+        data = response.json()
+        data['author'] = 'omicr0nn'
+        file_name = ipaddress + ".json"
+
+        if response.status_code == 200:
+            print("\033[35mRequests successfully")
+            try:
+                # Create and Save File
+                with open(file_name, "w") as json_file:
+                    json.dump(data, json_file, indent=4)
+                    print(f"\033[32mThe file named {file_name} has been saved")
+            except:
+                print("\033[31mFile creation failed")
+        else:
+            print("\033[31mRequest failed")
+        
+        countdown(3)
+    elif choose == 2:
+        clear_screen()
+        print("\033[35mGithub: \033[37mhttps://github.com/omicr0nn")
+        input("\n\033[31mIf you want to return to the main menu press enter...")
+    elif choose == 3:
+        print("Exiting... Thank you for use my application")
+        countdown(2)
+        clear_screen()
+        exit()
+        
 
 while True:
     ipİnfo()
